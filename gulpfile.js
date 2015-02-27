584f3785a002f3b1031a79e2783c5837c3fa8345
Cartowsky <mic.ferretti@gmail.com>
2015-02-27 18:54:37 +0100

    done with CSS & JS gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,74 @@
-var gulp = require('gulp'),
-	es = require('event-stream'),
-	concat = require('gulp-concat'),
-	ugliMin= require('gulp-uglifyjs'),
-	sass = require('gulp-ruby-sass'),
-	rename = require('gulp-rename'),
-	targetCssDir = 'styles',
-	gutil = require('gulp-util'),
-	autoprefix = require('gulp-autoprefixer');
+var gulp = require('gulp');
+var es = require('event-stream');
+var concat = require('gulp-concat');
+var ugliMin= require('gulp-uglifyjs');
+var sass = require('gulp-ruby-sass');
+var rename = require('gulp-rename');
+var gutil = require('gulp-util');
+var autoprefix = require('gulp-autoprefixer');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var watchify = require('watchify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
 
 
-// CSS e SASS
+/*--CSS e SASS--*/
 gulp.task('css', function(){
 
 	var vendorCSS = gulp.src(['node_modules/bootstrap/**/*.css','node_modules/leaflet-editinosm/**/*.css'] );
 	var mySASS = sass('app/sass/main.sass', { style: 'compressed' }).on('error', gutil.log);
 	
 	return es.concat(vendorCSS,mySASS)
-	.pipe(concat('app/styles.css'))
+	.pipe(concat('styles.css'))
 	.pipe(autoprefix('last 2 version'))
-	.pipe(gulp.dest(targetCssDir));
+	.pipe(gulp.dest('app/styles'));
 });
 
 
-// JS uno
-gulp.task('js-uno', function(){
-	var vendorScripts = gulp.src('app/vendor/*.js'),	
-		$ = require('jquery'),
-		Modernizr = require('modernizr'),
-		L = require('leaflet'),
-		LEditinosm = require('leaflet-editinosm');
-		
-	return es.ugliMin(vendorFiles, $, Modernizr, L, LEditinosm)
-	.pipe(gulp.rename('bundleUno.js'))
-	.pipe(gulp.dest('app/scripts'));
-});
+/*--Javascript--*/
 
-// JS due
-gulp.task('js-due',['js-uno'], function(){
-	var bundleUno = gulp.src('app/scripts/bundleUno.js');
-	var myScript = gulp.src('app/scripts/main.js')
+// JS-vendor
+gulp.task('js-vendor', function(){
 	
-	return es.ugliMin(bundleUno, myScript)
-	.pipe(gulp.rename('bundleTot.js'))
-	.pipe(gulp.dest('scripts'));
-
+	return gulp.src('./app/vendor/*.js')
+	.pipe(ugliMin())
+	.pipe(rename('bundleUno.js'))
+	.pipe(gulp.dest('./app/scripts/'));
 });
+
+// Broweserify
+var getBundleName = function () {
+  var version = require('./package.json').version;
+  var name = require('./package.json').name;
+  return version + '.' + name + '.' + 'min';
+};
+
+gulp.task('js', function() {
+
+  var bundler = browserify({
+    entries: ['./app/scripts/main.js'],
+    debug: true
+  });
+
+  var bundle = function() {
+    return bundler
+      .bundle()
+      .pipe(source(getBundleName() + '.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./app/scripts/dest'));
+  };
+
+  return bundle();
+});
+
